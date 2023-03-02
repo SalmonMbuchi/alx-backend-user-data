b725647ff9f934cd29153c8cec6d60ef397237c7
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-02 11:14:28 +0300

    last login details

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ from typing import List, Tuple
 
 
 PII_FIELDS: Tuple[str, str, str, str, str] = ('name', 'email',
-                                              'phone', 'ssn', 'last_login')
+                                              'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str,
