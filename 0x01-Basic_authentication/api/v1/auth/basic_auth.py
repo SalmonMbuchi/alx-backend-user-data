556490cece6d58b6e12fe32070aea701aa65c406
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-07 20:57:43 +0300

    pycodestyle fixes

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,7 +6,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic authentication"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """returns Base64 part of Authorization header"""
         if authorization_header is None:
             return None
