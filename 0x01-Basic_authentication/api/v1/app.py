a10e2d9004dc4cc1d0ddcf5c55045c8d2edcb53c
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-07 12:06:21 +0300

    pycodestyle fixes

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,18 +18,20 @@ if getenv("AUTH_TYPE") == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 def filter_req():
     """Filter each request"""
     if auth is None:
         pass
     else:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                                '/api/v1/forbidden/']
+                          '/api/v1/forbidden/']
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
+
 
 app.before_request(filter_req)
 
