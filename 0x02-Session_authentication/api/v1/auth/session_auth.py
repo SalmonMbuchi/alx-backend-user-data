5d0d47475deefa76e0474e6c3d74357f1afb41fc
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-08 21:26:50 +0300

    get user id via session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Session Authentication"""
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,13 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on cookie value"""
+        cookie_val = self.session_cookie(request)
+        if cookie_val:
+            user_id = self.user_id_for_session_id(cookie_val)
+            if user_id:
+                user = User.get(user_id)
+                if user:
+                    return user
