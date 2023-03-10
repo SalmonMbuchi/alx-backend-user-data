64f62eaf00d2d0e5729f98ee789c64e156630e7f
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-10 12:40:22 +0300

    session duration

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 
 class SessionExpAuth(SessionAuth):
     """adds expiration date to a session ID"""
+
     def __init__(self):
         try:
             self.session_duration = int(getenv('SESSION_DURATION'))
@@ -17,7 +18,8 @@ class SessionExpAuth(SessionAuth):
         """create a session id"""
         id = super().create_session(user_id)
         if id:
-            self.user_id_by_session_id[id] = {'user_id': user_id, 'created_at': datetime.now()}
+            self.user_id_by_session_id[id] = {
+                'user_id': user_id, 'created_at': datetime.now()}
             return id
         return None
 
@@ -29,10 +31,13 @@ class SessionExpAuth(SessionAuth):
         if session_dict:
             if self.session_duration <= 0:
                 return session_dict.get('user_id')
-            if session_dict.get('created_at') is None:
-                return None
-            current_time = session_dict.get('created_at') + self.session_duration
-            if current_time > timedelta():
-                return None
-            return session_dict.get('user_id')
+            else:
+                if session_dict.get('created_at') is None:
+                    return None
+                created_time = session_dict.get(
+                    'created_at') + timedelta(seconds=self.session_duration)
+                if created_time < datetime.now():
+                    return None
+                else:
+                    return session_dict.get('user_id')
         return None
