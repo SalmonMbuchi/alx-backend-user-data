9df43d94ad797e0cd8d7707dab38d3dd564078e0
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-10 10:57:33 +0300

    update from previous project

--- 0x02-Session_authentication/api/v1/auth/basic_auth.py
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -41,9 +41,8 @@ class BasicAuth(Auth):
             return (None, None)
         if ':' not in decoded_base64_authorization_header:
             return (None, None)
-        email = decoded_base64_authorization_header.split(":")
-        password = decoded_base64_authorization_header[len(email) + 1:]
-        return (email, password)
+        split_header = decoded_base64_authorization_header.split(":", 1)
+        return (split_header[0], split_header[1])
 
     def user_object_from_credentials(self, user_email: str,
                                      user_pwd: str) -> TypeVar('User'):
