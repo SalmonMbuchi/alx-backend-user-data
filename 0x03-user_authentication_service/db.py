f613ea75fea83e5483a81dc529e5a8fde1f69b3e
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-14 21:57:33 +0300

    validate attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,10 +46,9 @@ class DB:
         Return:
             - first row as filtered by kwargs
         """
-        # keys = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
-        key = list(kwargs.keys())[0]
-        if not hasattr(User, key):
-            raise InvalidRequestError
+        for key in kwargs.keys():
+            if not hasattr(User, key):
+                raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
             raise NoResultFound
