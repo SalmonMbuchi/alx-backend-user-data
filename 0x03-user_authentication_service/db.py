e37093acdba06c25bbb9c67e7ec81bd91c72336b
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-14 21:49:09 +0300

    find user by attributes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,10 +49,10 @@ class DB:
         keys = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
         key = list(kwargs.keys())[0]
         if key not in keys:
-            raise InvalidRequestError
+            raise InvalidRequestError()
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
-            raise NoResultFound
+            raise NoResultFound()
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
