9870fa3a894dea4dca48549984f71f5106069625
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-14 14:12:41 +0300

    register a user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,26 +17,26 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(encoded, bcrypt.gensalt())
 
 
-# class Auth:
-#     """Auth class to interact with the authentication database.
-#     """
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-#     def __init__(self):
-#         self._db = DB()
+    def __init__(self):
+        self._db = DB()
 
-#     def register_user(self, email: str, password: str) -> User:
-#         """
-#         Hash the password, save user to database
-#         Return:
-#             - User object
-#         """
-#         try:
-#             user = self._db.find_user_by(email=email)
-#             if user:
-#                 raise ValueError(f'User {email} already exists')
-#         except NoResultFound:
-#             hashed = _hash_password(password)
-#             return self._db.add_user(email, hashed.decode('utf-8'))
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Hash the password, save user to database
+        Return:
+            - User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            hashed = _hash_password(password)
+            return self._db.add_user(email, hashed.decode('utf-8'))
 
 #     def valid_login(self, email: str, password: str) -> bool:
 #         """
