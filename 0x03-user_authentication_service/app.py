ea26ba6ab4b919025236a4f4119c7c9ab59ce386
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-14 19:05:55 +0300

    pycodestyle fixes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,52 +35,52 @@ def users():
         return jsonify({'message': 'email already registered'}), 400
 
 
-# @app.route('/sessions', methods=['POST'], strict_slashes=False)
-# def login():
-#     """
-#     Login a user
-#     """
-#     email = request.form.get('email')
-#     if not email:
-#         return None
-#     password = request.form.get('password')
-#     if not password:
-#         return None
-#     if AUTH.valid_login(email, password):
-#         session_id = AUTH.create_session(email)
-#         cookie = jsonify({'email': email, 'message': 'logged in'})
-#         cookie.set_cookie('session_id', session_id)
-#         return cookie
-#     abort(401)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """
+    Login a user
+    """
+    email = request.form.get('email')
+    if not email:
+        return None
+    password = request.form.get('password')
+    if not password:
+        return None
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        cookie = jsonify({'email': email, 'message': 'logged in'})
+        cookie.set_cookie('session_id', session_id)
+        return cookie
+    abort(401)
 
 
-# @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
-# def logout():
-#     """
-#     Log out a user
-#     """
-#     session_id = request.cookies.get('session_id')
-#     if not session_id:
-#         abort(403)
-#     user = AUTH.get_user_from_session_id(session_id)
-#     if not user:
-#         abort(403)
-#     AUTH.destroy_session(user.id)
-#     return redirect(url_for('index'))
+@app.route('/sessions', methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    Log out a user
+    """
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect(url_for('index'))
 
 
-# @app.route('/profile', methods=['GET'], strict_slashes=False)
-# def profile():
-#     """
-#     user profile
-#     """
-#     session_id = request.cookies.get('session_id')
-#     if not session_id:
-#         abort(403)
-#     user = AUTH.get_user_from_session_id(session_id)
-#     if user is None:
-#         abort(403)
-#     return jsonify({'email': user.email}), 200
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """
+    user profile
+    """
+    session_id = request.cookies.get('session_id')
+    if not session_id:
+        abort(403)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({'email': user.email}), 200
 
 
 AUTH = Auth()
