f16c8e384b8629f0417246551cece123d15849f4
SalmonMbuchi <salmonmbuchi@gmail.com>
2023-03-17 21:47:16 +0300

    delete unwanted files

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -9,25 +9,31 @@ def register_user(email: str, password: str) -> None:
     """
     Register a user
     """
-    r = requests.post('http://localhost:5000/users', data={'email': email, 'password': password})
+    r = requests.post('http://localhost:5000/users',
+                      data={'email': email, 'password': password})
     assert r.status_code == 200
     assert r.json() == {'email': email, 'message': 'user created'}
+
 
 def log_in_wrong_password(email: str, password: str) -> None:
     """
     Validate output for wrong password
     """
-    r = requests.post('http://localhost:5000/sessions', data={'email': email, 'password': password})
+    r = requests.post('http://localhost:5000/sessions',
+                      data={'email': email, 'password': password})
     assert r.status_code == 401
+
 
 def log_in(email: str, password: str) -> str:
     """
     Test login
     """
-    r = requests.post('http://localhost:5000/sessions', data={'email': email, 'password': password})
+    r = requests.post('http://localhost:5000/sessions',
+                      data={'email': email, 'password': password})
     assert r.json() == {'email': email, 'message': 'logged in'}
-    session_id = r.cookies.get('session_id') 
-    return session_id 
+    session_id = r.cookies.get('session_id')
+    return session_id
+
 
 def profile_unlogged() -> None:
     """
@@ -36,14 +42,16 @@ def profile_unlogged() -> None:
     r = requests.get('http://localhost:5000/profile')
     assert r.status_code == 403
 
+
 def profile_logged(session_id: str) -> None:
     """
     Test profile endpoint when user is logged in
     """
     cookie = {'session_id': session_id}
     r = requests.get('http://localhost:5000/profile', cookies=cookie)
-    assert r.status_code == 200 
+    assert r.status_code == 200
     assert r.json() == {'email': 'guillaume@holberton.io'}
+
 
 def log_out(session_id: str) -> None:
     """
@@ -53,24 +61,29 @@ def log_out(session_id: str) -> None:
     r = requests.delete('http://localhost:5000/sessions', cookies=cookie)
     assert r.status_code == 200
 
+
 def reset_password_token(email: str) -> str:
     """
     Test resetting password token
     """
-    r = requests.post('http://localhost:5000/reset_password', data={'email': email})
+    r = requests.post('http://localhost:5000/reset_password',
+                      data={'email': email})
     assert r.status_code == 200
     token = r.json().get('reset_token')
     assert r.json() == {'email': email, 'reset_token': token}
     return token
 
+
 def update_password(email: str, reset_token: str, new_password: str) -> None:
     """
     Test password update
     """
-    payload={'email': email, 'reset_token': reset_token, 'new_password': new_password} 
+    payload = {'email': email, 'reset_token': reset_token,
+               'new_password': new_password}
     r = requests.put('http://localhost:5000/reset_password', data=payload)
     assert r.status_code == 200
     assert r.json() == {'email': email, 'message': 'Password updated'}
+
 
 EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
